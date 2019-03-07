f0c8c9a8da0d37cd03f5a3e2b3bab6ed764b4424
CWYH <cwyhxq@gmail.com>
2019-03-07 20:35:00 +0800

    唉

---
+++ 数值分析/python/Chapter12/SVD.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from skimage import io
+
+def getImgAsMat(index):
+    ds = datasets.fetch_olivetti_faces()
+    return np.mat(ds.images[index])
+
+def getImgAsMatFromFile(filename):
+    img = io.imread(filename, as_grey=True)
+    return np.mat(img) 
+
+def plotImg(imgMat):
+    plt.imshow(imgMat, cmap=plt.cm.gray)
+    plt.show()
+
+def recoverBySVD(imgMat, k):
+    # singular value decomposition
+    U, s, V = la.svd(imgMat)
+    # choose top k important singular values (or eigens)
+    Uk = U[:, 0:k]
+    Sk = np.diag(s[0:k])
+    Vk = V[0:k, :]
+    # recover the image
+    imgMat_new = Uk * Sk * Vk
+    return imgMat_new
+
+
+# -------------------- main --------------------- #
+A = getImgAsMatFromFile('C:/Users/CWYH/Pictures/Saved Pictures/1528625796608.jpg')
+print(A.shape)
+# plotImg(A)
+A_new = recoverBySVD(A, 100)
+plotImg(A_new)
